d0dadf1c30dbec3fb780e7ae764ca541f95b658d
alex90o <alex10o@hotmail.com>
2022-06-14 09:58:43 -0300

    se comentaron ejemplos anteriores

--- Introduccion/scrip.js
+++ Introduccion/scrip.js
@@ -3,36 +3,36 @@
 //FIXME:
 
 //Number
-console.log(3);
-console.log(3.5);
-console.log(typeof 3);
-console.log(typeof Infinity);
-console.log(typeof NaN);
+// console.log(3);
+// console.log(3.5);
+// console.log(typeof 3);
+// console.log(typeof Infinity);
+// console.log(typeof NaN);
 
-//String
-console.log('Hola Mundo');
-console.log ("la casa esta en orden");
-console.log(typeof "3");
-console.log("3");
+// //String
+// console.log('Hola Mundo');
+// console.log ("la casa esta en orden");
+// console.log(typeof "3");
+// console.log("3");
 
-//Booleanos
-console.log(typeof true);
-console.log(typeof false);
+// //Booleanos
+// console.log(typeof true);
+// console.log(typeof false);
 
-//Null y Undefined
-console.log(undefined);
-console.log(null);
+// //Null y Undefined
+// console.log(undefined);
+// console.log(null);
 
-console.log(typeof undefined);
-console.log(typeof null);
+// console.log(typeof undefined);
+// console.log(typeof null);
 
-let miVariable;
+// let miVariable;
 
-console.log(miVariable);
+// console.log(miVariable);
 
-miVariable = null;
-console.log(miVariable);
+// miVariable = null;
+// console.log(miVariable);
 
-miVariable = 3;
-console.log(miVariable);
-console.log(typeof miVariable);
+// miVariable = 3;
+// console.log(miVariable);
+// console.log(typeof miVariable);
