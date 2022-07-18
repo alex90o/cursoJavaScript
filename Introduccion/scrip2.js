8e0ad45679da36d3b408f4235e746c2438cadbd7
Alexis <alex10o@hotmail.com>
2022-07-18 20:54:01 -0300

    if / else / switch

---
+++ Introduccion/scrip2.js
@@ -0,0 +1,49 @@
+//CONTROL DE FLUJO 
+
+//IF / ELSE
+/*
+let age = 18;
+
+if (age >= 18) {
+    console.log("Podes manejer");
+}
+else {
+    console.log("No podes manejar");
+}
+*/
+
+//ELSE IF
+/*
+ let number = 10;
+
+ if (number < 10) {
+    console.log("El numero es menor a 10");
+ } else if (number > 10) {
+    console.log("El numero es mayor a 10");
+ }else {
+    console.log("El numero es igual a 10");
+ }
+ */
+
+ //SWITCH
+
+ //const weather = "DESPEJADO";
+ const weather = "fresCO";
+
+ switch (weather.toUpperCase()) { //para normalizaer la informacion
+    case "FRESCO":
+        console.log("LLeva Abrigo");
+        break;
+    
+    case "LLUVIOSO":
+        console.log("Cuidate del Sol");
+        break;
+    
+    case "SOLEADO":
+        console.log("Cuidate del Sol");
+        break;
+    
+    default:
+        console.log("El tiempo esta bueno. Disfruta del dia");
+        break;
+ }
