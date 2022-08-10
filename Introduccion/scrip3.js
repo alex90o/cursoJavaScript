a78ede4177c62aac9e7026d45afa4bb8b8c38bcc
alex90o <alex10o@hotmail.com>
2022-08-10 10:14:55 -0300

    funcion suma

---
+++ Introduccion/scrip3.js
@@ -0,0 +1,16 @@
+//Funciones
+console.log(10+9);
+console.log(9+9);
+console.log(1+7);
+console.log(3+6);
+
+//Declareción de la función
+function suma(n1, n2) {
+    console.
+    log(n1 + n2);
+}
+
+// Invocar la función (llamer función)
+
+suma (4, 8);
+
