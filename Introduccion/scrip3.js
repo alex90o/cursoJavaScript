f3f051c3eebe9c640ccff15e2b59cc9827516609
alex90o <alex10o@hotmail.com>
2022-08-10 10:19:16 -0300

    funcion sumaAlert

--- Introduccion/scrip3.js
+++ Introduccion/scrip3.js
@@ -1,16 +1,23 @@
 //Funciones
+/*
 console.log(10+9);
 console.log(9+9);
 console.log(1+7);
 console.log(3+6);
-
+*/
 //Declareción de la función
 function suma(n1, n2) {
     console.
     log(n1 + n2);
 }
 
+function sumaAlert(n1, n2) {
+    alert("El resultado de la suma es: "+ (n1 + n2));
+}
+
 // Invocar la función (llamer función)
 
 suma (4, 8);
+suma (30 ,56);
 
+sumaAlert (4, 8);
